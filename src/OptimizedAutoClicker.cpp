d3e18ccbb8b14103cc6e625d98f14e22ad40a8b3
fivepandasna <ashaandalgliesh@gmail.com>
2025-12-24 14:26:24 -0500

    refactor to use unicode for msvc build

--- src/OptimizedAutoClicker.cpp
+++ src/OptimizedAutoClicker.cpp
@@ -20,7 +20,7 @@ std::atomic<bool> g_running(false);
 std::atomic<bool> g_recordingKey(false);
 std::thread g_clickThread;
 int g_startStopKey = VK_F6;
-std::string g_keyName = "F6";
+std::wstring g_keyName = L"F6";
 std::mt19937 g_rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
 // Modern colors
@@ -36,68 +36,68 @@ HBRUSH g_inputBrush = CreateSolidBrush(INPUT_BG);
 HFONT g_font;
 
 // Get key name from virtual key code
-std::string GetKeyName(int vk) {
+std::wstring GetKeyName(int vk) {
     switch (vk) {
-        case VK_F1: return "F1";
-        case VK_F2: return "F2";
-        case VK_F3: return "F3";
-        case VK_F4: return "F4";
-        case VK_F5: return "F5";
-        case VK_F6: return "F6";
-        case VK_F7: return "F7";
-        case VK_F8: return "F8";
-        case VK_F9: return "F9";
-        case VK_F10: return "F10";
-        case VK_F11: return "F11";
-        case VK_F12: return "F12";
-        case VK_SPACE: return "Space";
-        case VK_RETURN: return "Enter";
-        case VK_ESCAPE: return "Esc";
-        case VK_TAB: return "Tab";
-        case VK_BACK: return "Backspace";
+        case VK_F1: return L"F1";
+        case VK_F2: return L"F2";
+        case VK_F3: return L"F3";
+        case VK_F4: return L"F4";
+        case VK_F5: return L"F5";
+        case VK_F6: return L"F6";
+        case VK_F7: return L"F7";
+        case VK_F8: return L"F8";
+        case VK_F9: return L"F9";
+        case VK_F10: return L"F10";
+        case VK_F11: return L"F11";
+        case VK_F12: return L"F12";
+        case VK_SPACE: return L"Space";
+        case VK_RETURN: return L"Enter";
+        case VK_ESCAPE: return L"Esc";
+        case VK_TAB: return L"Tab";
+        case VK_BACK: return L"Backspace";
         default:
             if (vk >= 'A' && vk <= 'Z') {
-                return std::string(1, (char)vk);
+                return std::wstring(1, (wchar_t)vk);
             }
             if (vk >= '0' && vk <= '9') {
-                return std::string(1, (char)vk);
+                return std::wstring(1, (wchar_t)vk);
             }
-            return "Unknown";
+            return L"Unknown";
     }
 }
 
 // Update button text with current key
 void UpdateButtonText() {
-    std::string startText = "Start (" + g_keyName + ")";
-    std::string stopText = "Stop (" + g_keyName + ")";
-    SetWindowTextA(g_startBtn, startText.c_str());
-    SetWindowTextA(g_stopBtn, stopText.c_str());
+    std::wstring startText = L"Start (" + g_keyName + L")";
+    std::wstring stopText = L"Stop (" + g_keyName + L")";
+    SetWindowText(g_startBtn, startText.c_str());
+    SetWindowText(g_stopBtn, stopText.c_str());
     InvalidateRect(g_startBtn, NULL, TRUE);
     InvalidateRect(g_stopBtn, NULL, TRUE);
 }
 
 // Get interval in milliseconds
 int GetIntervalMs() {
-    char buf[32];
+    wchar_t buf[32];
     int hours = 0, mins = 0, secs = 0, ms = 0;
     
-    GetWindowTextA(g_hoursEdit, buf, 32);
-    hours = atoi(buf);
-    GetWindowTextA(g_minsEdit, buf, 32);
-    mins = atoi(buf);
-    GetWindowTextA(g_secsEdit, buf, 32);
-    secs = atoi(buf);
-    GetWindowTextA(g_msEdit, buf, 32);
-    ms = atoi(buf);
+    GetWindowText(g_hoursEdit, buf, 32);
+    hours = _wtoi(buf);
+    GetWindowText(g_minsEdit, buf, 32);
+    mins = _wtoi(buf);
+    GetWindowText(g_secsEdit, buf, 32);
+    secs = _wtoi(buf);
+    GetWindowText(g_msEdit, buf, 32);
+    ms = _wtoi(buf);
     
     return (hours * 3600000) + (mins * 60000) + (secs * 1000) + ms;
 }
 
 // Get random offset
 int GetRandomOffset() {
-    char buf[32];
-    GetWindowTextA(g_offsetEdit, buf, 32);
-    int maxOffset = atoi(buf);
+    wchar_t buf[32];
+    GetWindowText(g_offsetEdit, buf, 32);
+    int maxOffset = _wtoi(buf);
     if (maxOffset <= 0) return 0;
     
     std::uniform_int_distribution<int> dist(0, maxOffset);
@@ -173,8 +173,8 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
         if (g_recordingKey) {
             g_startStopKey = kbs->vkCode;
             g_keyName = GetKeyName(kbs->vkCode);
-            std::string btnText = "Press a key... [" + g_keyName + "]";
-            SetWindowTextA(g_hotkeyBtn, btnText.c_str());
+            std::wstring btnText = L"Press a key... [" + g_keyName + L"]";
+            SetWindowText(g_hotkeyBtn, btnText.c_str());
             g_recordingKey = false;
             
             // Schedule update for next message loop iteration
@@ -228,9 +228,9 @@ LRESULT CALLBACK ButtonProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             SetTextColor(hdc, TEXT_COLOR);
             SelectObject(hdc, g_font);
             
-            char text[64];
-            GetWindowTextA(hwnd, text, 64);
-            DrawTextA(hdc, text, -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+            wchar_t text[64];
+            GetWindowText(hwnd, text, 64);
+            DrawText(hdc, text, -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
             
             EndPaint(hwnd, &ps);
             return 0;
@@ -243,67 +243,67 @@ LRESULT CALLBACK ButtonProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
         case WM_CREATE: {
-            g_font = CreateFontA(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+            g_font = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
                 DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-                CLEARTYPE_QUALITY, DEFAULT_PITCH, "Segoe UI");
+                CLEARTYPE_QUALITY, DEFAULT_PITCH, L"Segoe UI");
             
             // Create labels and inputs
             int y = 20;
-            CreateWindowExA(0, "STATIC", "Click Interval", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Click Interval", WS_CHILD | WS_VISIBLE,
                 20, y, 120, 20, hwnd, NULL, NULL, NULL);
             
             y += 30;
-            CreateWindowExA(0, "STATIC", "Hours:", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Hours:", WS_CHILD | WS_VISIBLE,
                 20, y, 50, 20, hwnd, NULL, NULL, NULL);
-            g_hoursEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0",
+            g_hoursEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"0",
                 WS_CHILD | WS_VISIBLE | ES_NUMBER, 75, y-2, 60, 24, hwnd, NULL, NULL, NULL);
             
-            CreateWindowExA(0, "STATIC", "Mins:", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Mins:", WS_CHILD | WS_VISIBLE,
                 145, y, 40, 20, hwnd, NULL, NULL, NULL);
-            g_minsEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0",
+            g_minsEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"0",
                 WS_CHILD | WS_VISIBLE | ES_NUMBER, 190, y-2, 60, 24, hwnd, NULL, NULL, NULL);
             
             y += 35;
-            CreateWindowExA(0, "STATIC", "Secs:", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Secs:", WS_CHILD | WS_VISIBLE,
                 20, y, 50, 20, hwnd, NULL, NULL, NULL);
-            g_secsEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0",
+            g_secsEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"0",
                 WS_CHILD | WS_VISIBLE | ES_NUMBER, 75, y-2, 60, 24, hwnd, NULL, NULL, NULL);
             
-            CreateWindowExA(0, "STATIC", "Ms:", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Ms:", WS_CHILD | WS_VISIBLE,
                 145, y, 40, 20, hwnd, NULL, NULL, NULL);
-            g_msEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "100",
+            g_msEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"100",
                 WS_CHILD | WS_VISIBLE | ES_NUMBER, 190, y-2, 60, 24, hwnd, NULL, NULL, NULL);
             
             y += 45;
-            CreateWindowExA(0, "STATIC", "Random Offset (0-N ms)", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Random Offset (0-N ms)", WS_CHILD | WS_VISIBLE,
                 20, y, 200, 20, hwnd, NULL, NULL, NULL);
             
             y += 25;
-            g_offsetEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "0",
+            g_offsetEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"0",
                 WS_CHILD | WS_VISIBLE | ES_NUMBER, 20, y, 230, 24, hwnd, NULL, NULL, NULL);
             
             y += 45;
-            CreateWindowExA(0, "STATIC", "Hotkey Settings", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Hotkey Settings", WS_CHILD | WS_VISIBLE,
                 20, y, 200, 20, hwnd, NULL, NULL, NULL);
             
             y += 25;
-            CreateWindowExA(0, "STATIC", "Start/Stop Key:", WS_CHILD | WS_VISIBLE,
+            CreateWindowEx(0, L"STATIC", L"Start/Stop Key:", WS_CHILD | WS_VISIBLE,
                 20, y, 110, 20, hwnd, NULL, NULL, NULL);
-            g_hotkeyBtn = CreateWindowExA(0, "BUTTON", "F6",
+            g_hotkeyBtn = CreateWindowEx(0, L"BUTTON", L"F6",
                 WS_CHILD | WS_VISIBLE | BS_OWNERDRAW, 135, y-2, 115, 28, hwnd, (HMENU)3, NULL, NULL);
             
             y += 45;
-            g_startBtn = CreateWindowExA(0, "BUTTON", "Start (F6)",
+            g_startBtn = CreateWindowEx(0, L"BUTTON", L"Start (F6)",
                 WS_CHILD | WS_VISIBLE | BS_OWNERDRAW, 20, y, 110, 35, hwnd, (HMENU)1, NULL, NULL);
             
-            g_stopBtn = CreateWindowExA(0, "BUTTON", "Stop (F6)",
+            g_stopBtn = CreateWindowEx(0, L"BUTTON", L"Stop (F6)",
                 WS_CHILD | WS_VISIBLE | BS_OWNERDRAW, 140, y, 110, 35, hwnd, (HMENU)2, NULL, NULL);
             
             EnableWindow(g_stopBtn, FALSE);
             
-            g_oldBtnProc = (WNDPROC)SetWindowLongPtrA(g_startBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
-            SetWindowLongPtrA(g_stopBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
-            SetWindowLongPtrA(g_hotkeyBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
+            g_oldBtnProc = (WNDPROC)SetWindowLongPtr(g_startBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
+            SetWindowLongPtr(g_stopBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
+            SetWindowLongPtr(g_hotkeyBtn, GWLP_WNDPROC, (LONG_PTR)ButtonProc);
             
             // Apply font to all children
             EnumChildWindows(hwnd, [](HWND child, LPARAM lParam) -> BOOL {
@@ -311,7 +311,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 return TRUE;
             }, 0);
             
-            g_hook = SetWindowsHookExA(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+            g_hook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
             break;
         }
         
@@ -328,13 +328,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             else if (LOWORD(wParam) == 2) StopClicking();
             else if (LOWORD(wParam) == 3) {
                 g_recordingKey = true;
-                SetWindowTextA(g_hotkeyBtn, "Press a key...");
+                SetWindowText(g_hotkeyBtn, L"Press a key...");
                 InvalidateRect(g_hotkeyBtn, NULL, TRUE);
             }
             break;
         
         case WM_USER + 1:
-            SetWindowTextA(g_hotkeyBtn, g_keyName.c_str());
+            SetWindowText(g_hotkeyBtn, g_keyName.c_str());
             UpdateButtonText();
             InvalidateRect(g_hotkeyBtn, NULL, TRUE);
             break;
@@ -349,25 +349,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             break;
         
         default:
-            return DefWindowProcA(hwnd, msg, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
 
 // Entry point
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
-    WNDCLASSEXA wc = {sizeof(WNDCLASSEX)};
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+    WNDCLASSEX wc = {sizeof(WNDCLASSEX)};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "AutoClickerClass";
+    wc.lpszClassName = L"AutoClickerClass";
     wc.hbrBackground = g_bgBrush;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPICON));
     wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPICON));
     
-    RegisterClassExA(&wc);
+    RegisterClassEx(&wc);
     
-    g_hwnd = CreateWindowExA(0, "AutoClickerClass", "Optimized AutoClicker",
+    g_hwnd = CreateWindowEx(0, L"AutoClickerClass", L"Optimized AutoClicker",
         WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
         CW_USEDEFAULT, CW_USEDEFAULT, 290, 380,
         NULL, NULL, hInstance, NULL);
